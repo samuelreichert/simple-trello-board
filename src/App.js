8e29f1af7bce716b89c996400a6d3fdce0ff2fb0
Samuel Reichert <samuelreichertt@gmail.com>
2021-03-07 00:06:00 -0300

    Update App removing code from Column component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import { useState } from 'react';
-import AddContent from './components/AddContent';
-import { fetchStorage, updateStorage } from './storage';
-import "./App.css";
+import Columns from './components/Columns';
 
-const App = () => {
-  const data = fetchStorage();
-  console.log({ columns: data });
-  const [columns, setColumns] = useState(data ?? []);
-  console.log(columns);
+import './App.css';
 
-  const onSaveColumn = (title) => {
-    const newColumns = [...columns];
-    newColumns.push({ title, cards: [] });
-    setColumns(newColumns);
-    updateStorage(newColumns);
-  };
+const App = () => (
+  <div
+    className='app'
+    style={{
+      backgroundImage: 'url(/trello-background.png)',
+      backgroundSize: 'cover',
+    }}
+  >
+    <header className='app__header'>
+      <h1 className='app__title'>Trello Board</h1>
+    </header>
 
-  return (
-    <div
-      className="app"
-      style={{
-        backgroundImage: `url(/trello-background.png)`,
-        backgroundSize: "cover",
-      }}
-    >
-      <header className="app__header">
-        <h1 className="app__title">Trello Board</h1>
-      </header>
-
-      <div className="columns-list">
-        {columns.map((column) => (
-          <div className="column">
-            <h4>{column.title}</h4>
-          </div>
-        ))}
-        <div className="column column--add">
-          <AddContent contentType="column" onSaveContent={onSaveColumn}/>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <Columns />
+  </div>
+);
 
 export default App;
